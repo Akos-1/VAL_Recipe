1e8d5f35982cec56b2b4a3098da71199d1516d67
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-05-02 20:13:44 +0000

    models updated

--- recipe-backend/models/Recipe.js
+++ recipe-backend/models/Recipe.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = require('../config/database');
+const db = require('../../config/database'); // Adjust the path as needed
 
 const Recipe = db.define('recipe', {
   title: {
--- recipe-backend/models/User.js
+++ recipe-backend/models/User.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = require('../config/database');
+const db = require('../../config/database'); // Adjust the path as needed
 
 const User = db.define('user', {
   username: {
