e98be71ca419f315d8f76b992158f60fcabb32b8
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-05-05 00:38:42 +0000

    fixing issues with registration

---
+++ recipe-backend/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+module.exports = {
+    development: {
+        username: 'val',
+        password: '1',
+        database: 'val',
+        host: 'localhost',
+        dialect: 'mysql'
+    },
+    // Add configurations for other environments if needed (e.g., production, testing)
+};
--- recipe-backend/server.js
+++ recipe-backend/server.js
@@ -1,3 +1,4 @@
+const Sequelize = require('sequelize');
 const mysql = require('mysql2/promise');
 const express = require('express');
 const bodyParser = require('body-parser');
