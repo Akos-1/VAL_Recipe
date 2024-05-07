5e3f271e21defa222dedc2e2f9a6e2e6f5a57833
Akos-1 <veronicagyasi@gmail.com>
2024-05-07 22:03:35 +0000

    Add tests for recipe search endpoint

---
+++ recipe-backend/unittests/search.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const app = require('../app');
+const assert = require('assert');
+
+describe('GET /recipes/search', () => {
+  it('should return recipes based on search query', (done) => {
+    const searchQuery = 'chicken';
+    request(app)
+      .get(`/recipes/search?search=${searchQuery}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert(res.body.hasOwnProperty('recipes'));
+        // Add more assertions for returned recipes if needed
+        done();
+      });
+  });
+});
