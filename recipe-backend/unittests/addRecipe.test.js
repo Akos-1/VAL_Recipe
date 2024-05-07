288a32731a3eecd14ffc17420b74fae45f3ed7dc
Akos-1 <veronicagyasi@gmail.com>
2024-05-07 22:06:10 +0000

    Include unit tests for adding a new recipe endpoint

---
+++ recipe-backend/unittests/addRecipe.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const app = require('../app');
+const assert = require('assert');
+
+describe('POST /recipes/add', () => {
+  it('should add a new recipe', (done) => {
+    const recipeData = { title: 'Test Recipe', ingredients: 'Ingredient 1, Ingredient 2', instructions: 'Step 1, Step 2' };
+    request(app)
+      .post('/recipes/add')
+      .send(recipeData)
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.strictEqual(res.body.message, 'Recipe added successfully');
+        done();
+      });
+  });
+});
